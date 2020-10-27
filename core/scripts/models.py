daef29960376d6f98a3fd107c1853fe830db547c
Adam Earle <robotlightsyou@gmail.com>
2020-10-27 00:28:13 -0700

    minor edits

--- core/scripts/models.py
+++ core/scripts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Tutorial(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class TextSection(models.Model):
+    content = models.TextField()
+
+class ImgSection(models.Model):
+    img = models.URLField()
