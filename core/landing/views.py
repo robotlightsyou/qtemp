3285acdb61eab8a2aa830b77a8f5f743d05294e4
Adam Earle <robotlightsyou@gmail.com>
2020-10-28 14:31:33 -0700

    landing structure

--- core/landing/views.py
+++ core/landing/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing(request):
+    return render(request, "landing/landing.html")
+
+def resume(request):
+    return render(request, "landing/resume.html", {'title': 'experience'})
+
+def contact(request):
+    return render(request, "landing/contact.html", {'title': 'contact us'})
+
+
+
